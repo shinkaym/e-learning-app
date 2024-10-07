351c75591ff6d7959ba128525e85e3a0b20676a1
Phạm Huỳnh Đăng Khoa <khoapham1405@gmail.com>
2024-10-07 11:12:51 +0700

    NextLink

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,38 +1,33 @@
-import { menuItems } from '@/constants'
-import React from 'react'
+import { menuItems } from '@/constants';
+import React from 'react';
+import ActiveLink from '../common/ActiveLink';
+import { TMenuItem } from '@/types';
 
 const Sidebar = () => {
   return (
     <div className='p-5 border-r border-r-gray-200'>
-      <a href='/' className="font-bold text-3xl inline-block mb-10">Ucademy</a>
+      <a href='/' className='font-bold text-3xl inline-block mb-5'>
+        <span className='text-primary'>U</span>
+        cademy
+      </a>
       <ul className='flex flex-col gap-2'>
-      {menuItems.map((item, index) => (
-        <MenuItem
-          key={index}
-          url={item.url}
-          title={item.title}
-          icon={item.icon}
-        />
-      ))}
+        {menuItems.map((item, index) => (
+          <MenuItem key={index} url={item.url} title={item.title} icon={item.icon} />
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-function MenuItem({
-  url,
-  title,
-  icon
-}: {
-  url: string,
-  title: string,
-  icon?: React.ReactNode
-}) {
+function MenuItem({ url, title, icon }: TMenuItem) {
   return (
     <li>
-      <a href={url} className='p-3 rounded-md flex items-center gap-3 hover:text-primary hover:bg-primary hover:bg-opacity-10 transition-all'>{icon}{title}</a>
+      <ActiveLink url={url}>
+        {icon}
+        {title}
+      </ActiveLink>
     </li>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
---
+++ src/types/index.tsx
@@ -0,0 +1,14 @@
+type ActiveLinkProps = {
+  url: string;
+  children: React.ReactNode;
+}
+
+type TMenuItem = {
+  url: string
+  title: string
+  icon: React.ReactNode
+}
+
+export type {
+  ActiveLinkProps, TMenuItem
+}
