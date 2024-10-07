ecb5d2f3b277a9d219a1305d963bb8f26a1bc6e8
Phạm Huỳnh Đăng Khoa <khoapham1405@gmail.com>
2024-10-07 20:53:15 +0700

    Connect DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { connectToDB } from '@/lib/mongoose';
+
+export default async function Home() {
+  const connect = connectToDB()
+  // console.log('🚀 ~ Home ~ connect:', connect)
   return (
     <div>HomePage</div>
   );
---
+++ src/lib/mongoose.ts
@@ -0,0 +1,28 @@
+'use server'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// singleton connection
+
+import mongoose from 'mongoose';
+
+let isConnected: boolean = false;
+
+export const connectToDB = async () => {
+  if(!process.env.MONGODB_URL) {
+    throw new Error('MONGODB_URL is not set');
+  }
+
+  if (isConnected) {
+    console.log('MONGODB is already connected')
+    return
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: 'ucademy'
+    })
+    isConnected = true
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.log('Error while connecting to MongoDB')
+  }
+}
