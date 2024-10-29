e9c53c078a734f2d8e1fc48ca63f7881382a1820
Phạm Huỳnh Đăng Khoa <khoapham1405@gmail.com>
2024-10-29 21:22:27 +0700

    Comment transition

--- src/app/(dashboard)/[course]/lesson/@comment/page.tsx
+++ src/app/(dashboard)/[course]/lesson/@comment/page.tsx
@@ -1,17 +1,29 @@
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-const page = () => {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getCourseBySlug } from '@/lib/actions/course.action';
+import { getLessonBySlug } from '@/lib/actions/lession.action';
+import CommentForm from './CommentForm';
+const page = async ({
+  params,
+  searchParams,
+}: {
+  params: {
+    course: string;
+  };
+  searchParams: {
+    slug: string;
+  };
+}) => {
+  const course = params.course;
+  const slug = searchParams.slug;
+  const findCourse = await getCourseBySlug({ slug: course });
+  if (!findCourse) return null;
+  const lesson = await getLessonBySlug({
+    slug: slug,
+    course: findCourse?._id.toString(),
+  });
   return (
     <div>
-      <div className="flex flex-col gap-5 mt-10">
-        <Textarea
-          placeholder="Enter your comment..."
-          className="min-h-[150px]"
-        />
-        <Button variant="primary" className="w-fit ml-auto">
-          Post comment
-        </Button>
-      </div>
+      <CommentForm></CommentForm>
       <div className="flex flex-col gap-5 mt-10">
         <h2 className="text-2xl font-bold">Comments</h2>
         <div className="flex flex-col gap-3">
--- src/app/(dashboard)/[course]/lesson/LessonWrapper.tsx
+++ src/app/(dashboard)/[course]/lesson/LessonWrapper.tsx
@@ -3,7 +3,7 @@
 import useGlobalStore from "@/store";
 import React from "react";
 const LessonWrapper = ({ children }: { children: React.ReactNode }) => {
-  const { expandedPlayer, setExpandedPlayer } = useGlobalStore();
+  const { expandedPlayer } = useGlobalStore();
   return (
     <div
       className="block xl:grid xl:grid-cols-[minmax(0,2fr),minmax(0,1fr)] gap-10 min-h-screen items-start"
